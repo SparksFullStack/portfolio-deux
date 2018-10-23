8c646898b5fbc20e17b86eeb8b3cbd55b9219382
SparksFullStack <csparks@antioch.edu>
2018-10-23 11:03:08 -0700

    colors and fonts added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import 'typeface-roboto';
 
 // components
 import TopMenu from './components/TopMenu';
